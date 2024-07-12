360080574da929b3ff15a1a0f5ab3f01f060cdb7
Prashanth-Netha <prashanthmittapally@outlook.com>
2024-07-12 19:29:32 +0530

    Add files via upload

---
+++ Delegates/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class Program
+{
+    // Declare a delegate
+    public delegate void Test(string message);
+
+    // Method that matches the delegate signature
+    public static void HelloMessage(string message)
+    {
+        Console.WriteLine("Hello, " + message);
+    }
+    public static void GreetMessage(string message)
+    {
+        Console.WriteLine("Good Morning, " + message);
+    }
+
+    public static void ByeMessage(string message)
+    {
+        Console.WriteLine("Bye, " + message);
+    }
+
+    public  class testprogram
+    {
+        Test t1;
+
+        public void test(Test t ) {
+            this.t1 = t;
+
+            t1("prashanth");
+         }
+    }
+
+    public static void Main()
+    {
+        // Instantiate the delegate
+        Test messageDelegate = new Test(HelloMessage);
+        messageDelegate += GreetMessage;
+        messageDelegate += ByeMessage;
+
+        testprogram testprogram = new testprogram();
+            testprogram.test(messageDelegate);
+
+        // Call the delegate
+        messageDelegate("World");
+    }
+}
